1a5f0c9ec68a67ecef6428024f944298868e6943
Hussain_Shahid <hussainshahid818@gmail.com>
2019-07-04 12:47:16 +0500

    subject class test modification

--- test/Unit/email/subject-test.php
+++ test/Unit/email/subject-test.php
@@ -20,17 +20,22 @@ class SubjectTest extends TestCase
         $this->expectExceptionMessage('Given string should not be greater then 998');
         $emailSubject = new Subject($actual);
     }
-    public function test_must_not_initialize_with_email_subject_each_line_greater_than_78_characters()
+    public function test_must_not_initialize_with_email_subject_each_line_greater_than_998_characters()
     {
+        $newSubject = str_repeat("a", 500);
         $actual = str_repeat("a", 999);
         echo <<<EOT
-        $actual
+        $newSubject
         $actual
        EOT;
         Subject::MAX_LENGTH;
         $this->expectExceptionCode(1004);
         $this->expectExceptionMessage('Given string should not be greater then 998');
         $emailSubject = new Subject($actual);
+        $newSubject1 = new Subject($newSubject);
+        $this->assertInstanceOf('jsys\types\email\Subject', $emailSubject);
+        $this->assertEquals('jsys\types\email\Subject', get_class($emailSubject));
+        $this->assertEquals($emailSubject->getSubject(), $newSubject1, 'Both must get the same value');
     }
     public function test_can_initialize_with_valid_subject_characters()
     {
